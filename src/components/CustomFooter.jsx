f09f81833901d611396c4dc7ccd3aef8c374ee35
Ariela Faivisovich Krowicki <138623058+arifaii@users.noreply.github.com>
2026-04-08 17:55:33 -0300

    arreglo footer

--- src/components/CustomFooter.jsx
+++ src/components/CustomFooter.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const CustomFooter = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="footer">
+    <footer className="footer">
       <p>&copy; {currentYear} Catálogo Geek. Todos los derechos reservados.</p>
-    </div>
+    </footer>
   );
 };
 
